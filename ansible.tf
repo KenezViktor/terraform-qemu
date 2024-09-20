f0795c0cb20e749545bd9fe0eff85539bbec0eac
Kenéz Viktor <kenezviktor98@gmail.com>
2024-09-20 21:31:27 +0200

    Config for ansible added

---
+++ ansible.tf
@@ -0,0 +1,26 @@
+resource "ansible_group" "sandbox" {
+  name = "sandbox-servers"
+  variables = {
+    ansible_user = "root"
+  }
+}
+
+resource "ansible_host" "sandbox" {
+  count = var.number_of_sandboxes
+  name = "${data.template_file.user_data-sandbox[count.index].vars.hostname}"
+  groups = [ ansible_group.sandbox.name ]
+}
+/*
+resource "ansible_playbook" "sandbox" {
+  count = var.number_of_sandboxes
+  playbook   = join("/", [var.ansible_playbook_path, "sandbox-servers.yml"])
+  name       = data.template_file.user_data-sandbox[count.index].vars.hostname
+  #groups = [ ansible_group.sandbox.name ]
+  groups = [ "sandbox-servers" ]
+  replayable = true
+  diff_mode = true
+  extra_vars = {
+    ansible_user = "root"
+  }
+}
+*/
