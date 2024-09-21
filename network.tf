3e3702d3a822e100afc61708f337e93672f5acb9
Kenéz Viktor <kenezviktor98@gmail.com>
2024-09-21 14:32:31 +0200

    Network for sandbox VMs added

---
+++ network.tf
@@ -0,0 +1,11 @@
+resource "libvirt_network" "sandbox" {
+  name = "sandbox"
+  mode = "nat"
+
+  domain = var.domain_name
+  addresses = [ join("" ,[var.sandbox-network.network, var.sandbox-network.netmask]) ]
+
+  dhcp {
+    enabled = false
+  }
+}
